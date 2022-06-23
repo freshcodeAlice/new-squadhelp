683eb958eac83c5cf1810074add09b04ac5fc005
freshcodeAlice <freshjs.ofte@gmail.com>
2022-06-23 19:30:32 +0300

    bug fixing

--- client/src/api/interceptor.js
+++ client/src/api/interceptor.js
@@ -2,28 +2,33 @@ import axios from 'axios';
 import CONTANTS from '../constants';
 import history from '../browserHistory';
 
-const instance = axios.create({
+const httpClient = axios.create({
   baseURL: CONTANTS.BASE_URL,
 });
 
-instance.interceptors.request.use((config) => {
-  const token = window.localStorage.getItem(CONTANTS.ACCESS_TOKEN);
+let accessToken;
+
+httpClient.interceptors.request.use((config) => {
+  const token = window.localStorage.getItem(CONTANTS.REFRESH_TOKEN);
   if (token) {
     config.headers = { ...config.headers, Authorization: `Bearer ${token}` };
   }
   return config;
 }, (err) => Promise.reject(err));
 
-instance.interceptors.response.use((response) => {
-  if (response.data.token) {
-    window.localStorage.setItem(CONTANTS.ACCESS_TOKEN, response.data.token);
+httpClient.interceptors.response.use((response) => {
+  if (response.data.data.refreshToken) {
+    window.localStorage.setItem(CONTANTS.REFRESH_TOKEN, response.data.data.refreshToken);
   }
   return response;
 }, (err) => {
-  if (err.response.status === 408 && history.location.pathname !== '/login' && history.location.pathname !== '/registration' && history.location.pathname !== '/') {
-    history.replace('/login');
+  if (err.response.status === 419) {
+   // todo: refresh
+  }
+  if (err.response.status === 401) {
+   // todo: redirect user to login/signup page
   }
   return Promise.reject(err);
 });
 
-export default instance;
+export default httpClient;
